87d816fad88243034db9138d3120838992e7087d
cdaringe <cdaringe@gmail.com>
2019-10-23 22:27:12 -0700

    fix(build): add notarization

---
+++ scripts/hooks/after-sign.ts
@@ -0,0 +1,42 @@
+// See: https://medium.com/@TwitterArchiveEraser/notarize-electron-apps-7a5f988406db
+import fs from 'fs'
+import path from 'path'
+import notarize from 'electron-notarize'
+
+const pkg = require('../../package.json')
+
+module.exports = async function maybeNotarize (params: any) {
+  if (process.platform !== 'darwin') {
+    return console.warn('skipping notarization, not OSX Darwin')
+  }
+  console.log('afterSign hook triggered', params)
+
+  const appBundleId: string = pkg.build.appId
+  if (!appBundleId) throw new Error('no appBundleId found in package.json')
+  const appPath = path.join(
+    params.appOutDir,
+    `${params.packager.appInfo.productFilename}.app`
+  )
+  if (!fs.existsSync(appPath)) {
+    throw new Error(`Cannot find application at: ${appPath}`)
+  }
+  console.log(`Notarizing ${appBundleId} found at ${appPath}`)
+  const appleId = process.env.AID
+  if (!appleId) throw new Error('no appleid found')
+  const appleIdPassword = process.env.AIP
+  if (!appleIdPassword) throw new Error('no appleIdPassword found')
+  await notarize
+    .notarize({
+      appBundleId,
+      appPath,
+      appleId,
+      appleIdPassword
+    })
+    .catch(err => {
+      console.error(
+        `failed to notarize: ${err ? err.message : 'NO ERROR MESSAGE FOUND'}`
+      )
+      throw err
+    })
+  console.log(`Done notarizing ${appBundleId}`)
+}

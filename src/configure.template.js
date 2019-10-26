d2405d14bc2adef36fb952751e38ae070034ff16
cdaringe <cdaringe@gmail.com>
2019-10-26 12:16:41 -0700

    chore: add more examples

--- src/configure.template.js
+++ src/configure.template.js
@@ -13,9 +13,33 @@ module.exports.configure = ({ fs, fetch }) => {
       }
     },
     {
+      pollDurationMs: 10000,
       name: 'long-running-tasks-show-pending',
       fn: async () => {
         return new Promise(res => setTimeout(res, 5000))
+      }
+    },
+    {
+      pollDurationMs: 10000 * 60,
+      name: 'current-air-quality',
+      fn: async ({ log }) => {
+        const zip = await execa('curl', ['http://ip-api.com/json']).then(res =>
+          parseInt(JSON.parse(res.stdout).zip)
+        )
+        return execa('curl', [
+          `https://airnow.gov/index.cfm?action=airnow.local_city&zipcode=${zip}&submit=Go`
+        ]).then(res => {
+          const html = res.stdout.toString()
+          const aqiMatch = html.match(/AQDataLg">([a-zA-Z]+)</)
+          if (!aqiMatch || !aqiMatch[1]) {
+            log({ level: 'error', message: 'no AQI found' })
+            log({ level: 'error', message: html })
+            throw new Error('unable to find aqi. see logs')
+          }
+          const level = aqiMatch[1]
+          if (level.toLowerCase().trim() === 'good') return true
+          throw new Error(`aqi is @ ${level}`)
+        })
       }
     }
   ]
